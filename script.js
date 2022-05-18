08d037785689bfc3a08109359161fc5c8a7f7afd
dalejefferson <66589962+dalejefferson@users.noreply.github.com>
2022-05-18 12:19:37 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+// DOM Manipulation
+
+//GetElementByID()
+// const title = document.getElementsByClassName('.right');
+// console.log(title);
+//GetElementByClass()
+
+//GetElementByTagName()
+
+//querySelector()
+
+//querySelectorAll()
+
+function funcYea(){
+   // Create an "li" node:
+   const e = document.createElement('div');
+   e.innerHTML = 'It Worked';
+   document.body.appendChild(e);
+   
+}
+
+   //   console.log(document.domain);
+      //   console.log(document.URL);
+      //   console.log(document.title);
+      //   document.title = 123;
+      //   console.log(document.doctype);
+      //   console.log(document.body);
+      //   console.log(window);
+      //   window.alert("HI");
+      //   var x = parseInt(prompt("Enter the Value of X"));
+      //   var y = parseInt(prompt("Enter the Value of y"));
+      //   console.log(x + y);
+      //   document.write(x + y);
+      //   a = confirm("Are you Sure");
+      //   console.log(a);
+      //   var a = window.innerWidth;
+      //   console.log(a);
+      //   var a = window;
+      //   console.log(a);
+
+      //   function funcYea() {
+      //     // let c = (document.getElementById("hi").innerHTML = "YO");
+      //     // alert(prompt("Enter name"));
+
+      //   }
+
+      //   var x = document;
+      //   console.log(x);
+      //   var x = document.all;
+      //   console.log(x);
+      //   var x = document.forms;
+      //   console.log(x);
+      //   var x = document.forms;
+      //   console.log(x);
+      //   var x = document.images;
+      //   console.log(x);
+      //   var x = document.scripts;
+      //   console.log(x);
+      //   var x = document.scripts;
+      //   console.log(x);
+
+      //   function func() {
+      //     let x = document;
+      //     x = document.querySelector(".right");
+      //     console.log(x);
+      //   }
+    //   function func() {
+    //     let x = document;
+    //     x = document.querySelector(".container");
+    //     let y = x.querySelector(".right");
+    //     console.log(x);
+    //   }
